97fc9e599faf4708ad4f13c5f93c2d6aa9f08fa0
ryosms <ryo.sms@gmail.com>
2020-01-24 23:53:38 +0900

    Add Login form

---
+++ internal/login/login_view.go
@@ -0,0 +1,38 @@
+package login
+
+import (
+	"fmt"
+	"github.com/aarzilli/nucular"
+)
+
+type loginData struct {
+	username string
+	token    string
+
+	usernameEditor nucular.TextEditor
+	tokenEditor    nucular.TextEditor
+}
+
+var login loginData
+
+func UpdateView(w *nucular.Window) {
+	w.Row(30).Dynamic(1)
+	w.Label("Login", "LT")
+
+	w.Row(30).Ratio(0.2, 0.8)
+
+	w.Label("username:", "RC")
+	login.usernameEditor.Flags = nucular.EditField
+	login.usernameEditor.Edit(w)
+	login.username = string(login.usernameEditor.Buffer)
+
+	w.Label("token:", "RC")
+	login.tokenEditor.Flags = nucular.EditField
+	login.tokenEditor.Edit(w)
+	login.token = string(login.tokenEditor.Buffer)
+
+	w.Row(30).Dynamic(1)
+	if w.ButtonText("Login") {
+		fmt.Printf("username: %s, token: %s\n", login.username, login.token)
+	}
+}
--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/aarzilli/nucular"
 	"github.com/aarzilli/nucular/style"
+	"github.com/ryosms/pixela-desktop/internal/login"
 )
 
-var count int
-
 func main() {
-	wnd := nucular.NewMasterWindow(0, "Pixela Desktop", updatefn)
+	wnd := nucular.NewMasterWindow(0, "Pixela Desktop", login.UpdateView)
 	wnd.SetStyle(style.FromTheme(style.DarkTheme, 1.5))
 	wnd.Main()
-}
-
-func updatefn(w *nucular.Window) {
-	w.Row(50).Dynamic(1)
-	if w.ButtonText(fmt.Sprintf("increment: %d", count)) {
-		count++
-	}
-
 }
