56963704cee9b228fddec22bec277b1bce2e8c67
ryosms <ryo.sms@gmail.com>
2020-02-29 23:36:16 +0900

    Impl show stats and definition

--- internal/graphs/detail.go
+++ internal/graphs/detail.go
@@ -11,7 +11,7 @@ type GraphDetailView struct {
 	username   string
 	graph      pixela.GraphDefinition
 	parent     *nucular.Window
-	stats      pixela.GraphStats
+	stats      *pixela.GraphStats
 	statsError string
 	img        *image.RGBA
 	imgError   string
@@ -29,7 +29,7 @@ func ShowDetail(w *nucular.Window, username string, graphDef pixela.GraphDefinit
 	w.Master().PopupOpen(graphDef.Name, 0, w.Bounds, false, updateDetailView)
 
 	go func() {
-		loadSvgImage("20190101")
+		loadSvgImage("")
 	}()
 
 	go func() {
@@ -38,20 +38,21 @@ func ShowDetail(w *nucular.Window, username string, graphDef pixela.GraphDefinit
 			detailView.statsError = "failed to get graph stats"
 			fmt.Printf("%+v\n", err)
 		} else {
-			detailView.stats = *stats
+			detailView.stats = stats
 		}
 	}()
 }
 
 func updateDetailView(w *nucular.Window) {
+	g := detailView.graph
 	w.Row(30).Static(50, 0)
 	if w.ButtonText("Back") {
 		w.Close()
 	}
-	w.Label(detailView.graph.Name, "LC")
+	w.Label(g.Name, "LC")
 
 	if detailView.img != nil {
-		w.Row(detailView.img.Rect.Max.Y).Dynamic(1)
+		w.Row(int(float64(detailView.img.Rect.Max.Y) / 1.5)).Dynamic(1)
 		w.Image(detailView.img)
 	}
 	if len(detailView.imgError) > 0 {
@@ -62,6 +63,20 @@ func updateDetailView(w *nucular.Window) {
 	if len(detailView.statsError) > 0 {
 		w.Row(40).Dynamic(1)
 		w.Label(detailView.statsError, "LT")
+	}
+	if detailView.stats != nil {
+		showStats(w, detailView.stats, g.Unit)
+	}
+
+	if w.TreePush(nucular.TreeTab, "Definition", false) {
+		w.Row(20).Dynamic(1)
+		w.Label(fmt.Sprintf("Unit: %s", g.Unit), "LT")
+		w.Label(fmt.Sprintf("Type: %s", g.Type), "LT")
+		w.Label(fmt.Sprintf("Timezone: %s", g.Timezone), "LT")
+		w.Label(fmt.Sprintf("SelfSufficient: %v", g.SelfSufficient), "LT")
+		w.Label(fmt.Sprintf("Purge Target: %d", len(g.PurgeCacheUrls)), "LT")
+
+		w.TreePop()
 	}
 
 	w.Bounds = detailView.parent.Bounds
@@ -87,5 +102,19 @@ func loadSvgImage(date string) {
 			detailView.img = img
 		}
 	}
+}
 
+func showStats(w *nucular.Window, st *pixela.GraphStats, unit string) {
+	if w.TreePush(nucular.TreeTab, "Stats", false) {
+		w.Row(30).Dynamic(1)
+		w.Label(fmt.Sprintf("Today: %v %s", st.TodaysQuantity, unit), "LT")
+
+		w.Row(20).Dynamic(1)
+		w.Label(fmt.Sprintf("Total: %v %s", st.TotalQuantity, unit), "LT")
+		w.Label(fmt.Sprintf("Max: %v %s", st.MaxQuantity, unit), "LT")
+		w.Label(fmt.Sprintf("Min: %v %s", st.MinQuantity, unit), "LT")
+		w.Label(fmt.Sprintf("Total Pixels: %v %s", st.TotalPixelsCount, unit), "LT")
+
+		w.TreePop()
+	}
 }
